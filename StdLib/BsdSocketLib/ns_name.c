9957e77a60c274b0646368b69aa642dbae38f690
Josh Soref <jsoref@users.noreply.github.com>
2021-08-22 12:29:49 -0400

    spelling: character
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- StdLib/BsdSocketLib/ns_name.c
+++ StdLib/BsdSocketLib/ns_name.c
@@ -531,7 +531,7 @@ ns_name_skip(const u_char **ptrptr, const u_char *eom) {
 /*
  * special(ch)
  *	Thinking in noninternationalized USASCII (per the DNS spec),
- *	is this characted special ("in need of quoting") ?
+ *	is this character special ("in need of quoting") ?
  * return:
  *	boolean.
  */
